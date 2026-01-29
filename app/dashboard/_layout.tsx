5e50d692560f888c6a152d59cad8290cd7ebbac4
Isaac Soto <jsl301003@gmail.com>
2026-01-28 21:34:00 -0700

    EMCT-2:Updated-dashboard-layout

--- app/dashboard/_layout.tsx
+++ app/dashboard/_layout.tsx
@@ -1,37 +1,7 @@
-import { View } from 'react-native';
-import { BottomNavigation } from 'react-native-paper';
-import { useRouter, useSegments, Slot, Stack } from 'expo-router';
-
-const routes = [
-  { key: 'cycle', title: 'Cycle', focusedIcon: 'sync' },
-  { key: 'calendar', title: 'Calendar', focusedIcon: 'calendar', unfocusedIcon: 'calendar-outline' },
-];
+import Dashboard from "../../src/components/layout/Dashboard";
 
 export default function DashboardLayout() {
-  const router = useRouter();
-  const segments = useSegments();
-
-  const current = segments[segments.length - 1];
-  const index = Math.max(
-    routes.findIndex(r => r.key === current),
-    0
-  );
-
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <Stack 
-          screenOptions={{
-            headerShown: false,
-          }} />
-      </View>
-
-      <BottomNavigation.Bar
-        navigationState={{ index, routes }}
-        onTabPress={({ route }) => {
-          router.replace(`/dashboard/${route.key}`);
-        }}
-      />
-    </View>
+    <Dashboard />
   );
 }
