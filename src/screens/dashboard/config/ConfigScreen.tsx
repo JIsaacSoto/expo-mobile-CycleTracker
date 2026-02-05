eae6e38c67c3ab344f68fa0477dd0b94d00fe499
Isaac Soto <jsl301003@gmail.com>
2026-02-05 00:06:42 -0700

    EMCT-3:Added-config-fields

--- src/screens/dashboard/config/ConfigScreen.tsx
+++ src/screens/dashboard/config/ConfigScreen.tsx
@@ -1,15 +1,46 @@
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { View } from "react-native";
+import React from "react";
+import { Text, TextInput, Divider } from "react-native-paper";
+import Button from "@/components/ui/Button";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { styles } from "./ConfigScreen.styles";
+import { router } from "expo-router";
 
 export default function ConfigScreen() {
+  const [lastPeriodDate, setLastPeriodDate] = React.useState("");
+  const [periodLength, setPeriodLength] = React.useState("");
+  const [cycleLength, setCycleLength] = React.useState("");
+
   return (
     <View style={styles.container}>
       <Text variant="headlineMedium" style={styles.title}>
         Config Screen
+        {"\n"}
       </Text>
-      <MaterialCommunityIcons name="cog" size={32} color="red" />
+      <View style={{ width: "80%", gap: 16 }}>
+        <TextInput
+          label="Last Period Date"
+          mode="outlined"
+          value={lastPeriodDate}
+          onChangeText={(text) => setLastPeriodDate(text)}
+        />
+        <TextInput
+          label="Period Length (days)"
+          mode="outlined"
+          value={periodLength}
+          onChangeText={(text) => setPeriodLength(text)}
+        />
+        <TextInput
+          label="Cycle Length (days)"
+          mode="outlined"
+          value={cycleLength}
+          onChangeText={(text) => setCycleLength(text)}
+        />
+        <Button
+          onPress={() => router.push("/dashboard/config")}
+          text="Get Started"
+        ></Button>
+      </View>
     </View>
   );
 }
